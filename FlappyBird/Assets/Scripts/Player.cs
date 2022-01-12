3b7f847bd6198e17aad98c0298da9a39e623b2c6
Baumel Márton <marcibaumel@gmail.com>
2022-01-12 11:30:11 +0100

    sprite animation

--- FlappyBird/Assets/Scripts/Player.cs
+++ FlappyBird/Assets/Scripts/Player.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
     private Vector3 direction;
     public float gravity = -9.8f;
     public float strength = 5f;
+
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void AnimateSprite(){
+        spriteIndex++;
+        if(spriteIndex >= sprites.Length){
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.Space)){
